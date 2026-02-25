a4fbc0839b02a2706494856f3396a9fcc782cd74
Jeffrey Morgan <jmorgan@flvc.org>
2026-02-25 10:30:03 -0500

    Removed an unneeded function call and made the equipment search case insensitive.

--- src/app/remove-equipment/remove-equipment.component.ts
+++ src/app/remove-equipment/remove-equipment.component.ts
@@ -26,20 +26,14 @@ export class RemoveEquipmentComponent implements OnInit {
       return;
     }
 
-    this.element = this.findClosestTargetInHost();
+    this.element = findClosestTargetFromElement(this.elementRef.nativeElement.parentElement, 'ng-star-inserted');
     if (this.element != null) {
       const h4 = findClosestTargetFromElement(this.element,'h4');
       if (h4 != null && h4.textContent != null) {
-        if (h4.textContent.includes('equipment')) {
+        if (h4.textContent.toLowerCase().includes('equipment')) {
           this.element.style.display = 'none';
         }
       }
     }
-  }
-
-  private findClosestTargetInHost(): HTMLElement | null {
-    let current: HTMLElement | null = this.elementRef.nativeElement.parentElement;
-
-    return findClosestTargetFromElement(current, 'ng-star-inserted')
   }
 }
