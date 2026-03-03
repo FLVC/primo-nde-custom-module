e396424a1d084983e24fd83f434ed0e937386a2e
Jeffrey Morgan <jmorgan@flvc.org>
2026-03-03 14:42:27 -0500

    Added support for multiple views.

--- src/app/remove-equipment/remove-equipment.component.ts
+++ src/app/remove-equipment/remove-equipment.component.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, Input, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, inject, Inject, OnInit } from '@angular/core';
 import { findClosestTargetFromElement } from '../shared/utils';
+import { Store } from '@ngrx/store';
+import { selectViewId } from '../primo-store.service';
+import { distinctUntilChanged, shareReplay, take } from 'rxjs';
 
 @Component({
   selector: 'custom-remove-equipment',
@@ -13,6 +16,12 @@ export class RemoveEquipmentComponent implements OnInit {
   @Input() private hostComponent!: any;
   element: HTMLElement | null = null;
   viewId: string = '';
+  public store = inject(Store);
+
+  readonly viewId$ = this.store.select(selectViewId).pipe(
+    distinctUntilChanged(),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
 
   constructor(
     private elementRef: ElementRef,
@@ -21,11 +30,24 @@ export class RemoveEquipmentComponent implements OnInit {
   
   ngOnInit(): void {
     const enabled = this.moduleParameters.removeEquipmentEnabled === "true";
+    const viewsParam = this.moduleParameters.removeEquipmentViews;
+    const views = viewsParam?.replace(/^\[|\]$/g, "").split(",").map((s: string) => s.trim());
 
     if (!enabled) {
       return;
     }
-    
+
+    this.viewId$
+      .pipe(take(1))
+      .subscribe(code => {
+        this.viewId = code ?? '';
+      });
+
+    if (views != undefined && !views.includes(this.viewId)) {
+      console.log('No matching view found for ' + this.viewId + ' options are ' + views)
+      return;
+    }
+
     if (this.hostComponent?.filterGroup?.id === 'lds01' && this.hostComponent?.filterValue?.value === 'Equipment') {
       this.element = findClosestTargetFromElement(this.elementRef.nativeElement.parentElement, 'ng-star-inserted');
       if (this.element != null) {
