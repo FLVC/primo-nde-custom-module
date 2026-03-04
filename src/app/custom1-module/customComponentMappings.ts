e4ada8f8786c9e43a332c2f08f2db9aaef87c4a6
Jeffrey Morgan <jmorgan@flvc.org>
2026-03-04 09:52:01 -0500

    Added the finding-aid NDE module.

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,3 +1,4 @@
+import { FindingAidComponent } from "../finding-aid/finding-aid.component";
 import { DigitizationLinksComponent } from "../digitization-links/digitization-links.component";
 import { FlvcBannerComponent } from "../flvc-banner/flvc-banner.component";
 import { HathiAvailabilityComponent } from "../hathi-availability/hathi-availability.component";
@@ -24,5 +25,5 @@ export const selectorComponentMap = new Map<string, any>([
 ['nde-header-after', NicheAcademyComponent],
 ['nde-physical-availability-line-bottom', DigitizationLinksComponent],
 ['nde-location-bottom', HideAvailabilityComponent]
-
+['nde-record-availability-bottom', FindingAidComponent]
 ]);
---
+++ src/app/finding-aid/finding-aid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-finding-aid',
+  standalone: true,
+  imports: [],
+  templateUrl: './finding-aid.component.html',
+  styleUrl: './finding-aid.component.scss'
+})
+export class FindingAidComponent {
+
+}
