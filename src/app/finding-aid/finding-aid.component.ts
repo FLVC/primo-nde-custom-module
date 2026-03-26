5031fbaf68e707ac91c4bf84b570d3518610cf40
Jeffrey Morgan <jmorgan@flvc.org>
2026-03-26 10:47:37 -0400

    Added code for selecting views and the finding aid and digitized material links to search for per institution.

--- src/app/finding-aid/finding-aid.component.ts
+++ src/app/finding-aid/finding-aid.component.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 import { selectFullDisplayRecord, selectListViewRecord } from '../primo-store.service';
 import { MatIconRegistry, MatIcon } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { selectViewId } from '../primo-store.service';
+import { distinctUntilChanged, shareReplay, take } from 'rxjs';
 
 @Component({
   selector: 'custom-finding-aid',
@@ -23,7 +25,12 @@ export class FindingAidComponent implements OnInit {
   isFullRecord: boolean = false;
   findingAidLink: string = "";
   digitizedMaterialLink: string = "";
-  
+
+  readonly viewId$ = this.store.select(selectViewId).pipe(
+    distinctUntilChanged(),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+
   constructor(
     @Inject('MODULE_PARAMETERS') public moduleParameters: any,
     private matIconRegistry: MatIconRegistry,
@@ -37,16 +44,31 @@ export class FindingAidComponent implements OnInit {
   
   ngOnInit(): void {
     const enabled = this.moduleParameters.findingAidEnabled === "true";
+    const findingAidArray:string[] = this.moduleParameters.findingAidArray;
+    const digitizedMaterialArray:string[] = this.moduleParameters.digitizedMaterialArray;
+    const viewsParam = this.moduleParameters.findingAidViews;
+    const views = viewsParam?.replace(/^\[|\]$/g, "").split(",").map((s: string) => s.trim());
     
     if (!enabled) {
       return;
     }
 
+    this.viewId$
+      .pipe(take(1))
+      .subscribe(code => {
+        this.viewId = code ?? '';
+      });
+
+    if (views != undefined && !views.includes(this.viewId)) {
+      console.log('No matching view found for ' + this.viewId + ' options are ' + views)
+      return;
+    }
+    
     this.record$ = this.store.select(selectFullDisplayRecord);
     this.record$.subscribe((record) => {
       if (record) {
         this.isFullRecord = true;
-        this.findRecordLinks(record);
+        this.findRecordLinks(record, findingAidArray, digitizedMaterialArray);
       }
       else {
         this.isFullRecord = false;
@@ -55,20 +77,21 @@ export class FindingAidComponent implements OnInit {
 		if (!this.isFullRecord) {
 			this.record$ = this.store.select(selectListViewRecord(this.hostComponent?.searchResult?.pnx?.control?.recordid[0]));
 			this.record$.subscribe((record) => {
-				this.findRecordLinks(record);
+				this.findRecordLinks(record, findingAidArray, digitizedMaterialArray);
 			});
 		}        
   }
 
-  findRecordLinks = (record: any): any => {
+  findRecordLinks = (record: any, findingAidArray:string[], digitizedMaterialArray:string[]): any => {
     if (record) {
       if (record?.pnx?.display) {
         Object.entries(record?.pnx?.display).forEach((key, value) => {
           const compareValue = String(key[1]);
-          if (compareValue.includes("Finding aid") || compareValue.includes("Guía de la descripción de los documentos (Spanish-language finding aid):") || compareValue.includes("Guía de los Documentos (Spanish-language finding aid)")) {
+
+          if (compareValue.includes("Finding aid") || findingAidArray?.some(item => compareValue.includes(item))) {
             this.findingAidLink = compareValue.substring(compareValue.indexOf(':')+1).trim();
           }
-          if (compareValue.includes("Digitized material") || compareValue.includes("Items from this collection have been digitized and are available online)")) {
+          if (compareValue.includes("Digitized material") || digitizedMaterialArray?.some(item => compareValue.includes(item))) {
             this.digitizedMaterialLink = compareValue.substring(compareValue.indexOf(':')+1);
           }
         });
